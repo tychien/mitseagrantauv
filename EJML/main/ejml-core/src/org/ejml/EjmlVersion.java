6dbee33df5985ad0c22ffa98ddc53d0db619c826
tychien <tychien@gmail.com>
2020-03-06 13:41:21 -0500

    EJML

---
+++ EJML/main/ejml-core/src/org/ejml/EjmlVersion.java
@@ -0,0 +1,19 @@
+package org.ejml;
+
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class EjmlVersion {
+	public static final String MAVEN_GROUP = "org.ejml";
+	public static final String MAVEN_NAME = "ejml";
+	public static final String VERSION = "0.39-SNAPSHOT";
+	public static final int GIT_REVISION = 775;
+	public static final String GIT_SHA = "21f86dbc5efd83856a206f38920bd44e93582998";
+	public static final String GIT_DATE = "2019-12-16T00:26:38Z";
+	public static final String BUILD_DATE = "2020-03-02T17:29:15Z";
+	public static final long BUILD_UNIX_TIME = 1583170155474L;
+	public static final int DIRTY = 0;
+
+	private EjmlVersion(){}
+}
