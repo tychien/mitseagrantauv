660f84d095fb31cdf8bdcc561ad2c5ab470570ff
tychien <a9114242@icloud.com>
2021-12-13 09:14:52 +0800

    Update count.py

--- ais/AIS_gui/count.py
+++ ais/AIS_gui/count.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from csv import DictReader, DictWriter
 from datetime import datetime
 def countShip(readpath):
@@ -21,28 +22,31 @@ def countShip(readpath):
                 pass
             else:
                 datelist.append(date)
-                
+
             if mmsi in mmsilist:
                 pass
             else:
                 mmsilist.append(mmsi)
-       
-        
+
+
         datedict = {key:[] for key in datelist}
-        
+
         for ship in shiplist:
             record_time_b = datetime.strptime(ship['Record_Time'],dtformat)
             year    = record_time_b.year
             month   = record_time_b.month
             day     = record_time_b.day
             date    = str(year)+'{:02d}'.format(month)+'{:02d}'.format(day)
-            datedict[date].append(ship)     
-        
+            datedict[date].append(ship)
+
         for date in datedict:
             for i in range(len(datedict[date])+1):
                 print(datedict[date][i-1]['Record_Time'])
+
+
+
     #print((datedict['20210915'][19]['Record_Time']))
     print(len(shiplist))
 
 if __name__ == '__main__':
-    countshipt = countShip('/home/tychien/mitseagrantauv/ais/AIS_gui/CHe1_5K.csv')
+    countshipt = countShip(str(Path.home())+'/mitseagrantauv/ais/AIS_gui/CHe1_5K.csv')
